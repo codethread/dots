4576ef14ab5f1f1e845012799604712b6627b228
codethread <adamhalldesigns@gmail.com>
2026-07-27 08:09:34 +0100

    feat(nvim): load Oil as default explorer

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -84,11 +84,13 @@ return {
 
 	{
 		'stevearc/oil.nvim',
+		lazy = false,
 		cmd = 'Oil',
 		opts = {
 			default_file_explorer = true,
 			view_options = {
 				show_hidden = true,
+				compact_directories = true,
 			},
 			columns = {
 				'icon',
